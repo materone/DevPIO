40cae6f6d771102805d8cb7c888ec460f55205af
-l <materone.mac@gmail.com>
2016-10-29 15:22:19 +0000

    update for 16bit quick display

--- src/dumphz.ino
+++ src/dumphz.ino
@@ -16,7 +16,7 @@ SPIFlash flash(PIN_FLASH_CS);
 uint32_t pos = 0;
 
 // Modify the line below to match your display and wiring:
-// HSPI 4口 12，13，14，15(CS)
+// HSPI 4pin 12,13,14,15(CS)
 // DC/RS 4
 // RST  5
 // LED(Back Light) PullUp (0 or V3.3)
@@ -35,9 +35,12 @@ UTFT myGLCD(ILI9341_S5P, PIN_TFT_CS, 5, 4);
 #define F24L  F16L + 498528 + 14 //760430
 
 union {
-  uint8_t buff[9360];
-  uint8_t fbuff[3120][3];
-} ubuff;
+  uint8_t buff[9600];//9300 for 24,9600 for 16
+  uint8_t fbuff24[3120][3];//for 24
+  uint8_t fbuff16[4800][2];//for 16
+} ubuff;//Font Data buffer
+
+uint8_t buff[15360];//Display RAM Buffer: 15360 for 16;11520 for 24 px
 
 void setup() {
   // put your setup code here, to run once:
@@ -124,10 +127,96 @@ void dumpFont16() {
 }
 
 
+void dumpFont16Quick() {
+  uint16_t x = 0, y = 0;
+  pos = 14 ;
+  unsigned long start = millis();
+  while (pos < 261710) {
+    //    Serial.println(pos);
+    sbi(myGLCD.P_CS, myGLCD.B_CS);
+    delay(5);
+    FLASH_CS_ON;
+    delay(5);
+    if (!flash.readByteArray(pos, ubuff.buff, 9600)) {
+      Serial.println(F("Read Flash Error"));
+    }
+    //    for(uint8_t m=0;m<32;m++){
+    //      Serial.print(buff[m],HEX);
+    //    }
+    //    Serial.print("    |");
+    //    Serial.println(c);
+    pos += 9600;
+    delay(40);
+    FLASH_CS_OFF;
+    cbi(myGLCD.P_CS, myGLCD.B_CS);
+    //    myGLCD.setXY((c%15)*16,(c/15)*16,15,15);
+    for (uint16_t m = 0; m < 300; m++) {
+      uint16_t mChars = m * 16;
+      //      uint16_t linePointX = (m % 10) * 24;
+      uint16_t linePointY = (m / 30) * 32;
+      //      uint16_t ml = 0;
+      uint16_t b1 = (m % 15) * 32;//(m % 15) * 32;
+      if((m/15)%2 == 1) b1 += 7680;
+      if(m % 30 == 0){
+        myGLCD.setXY(0, linePointY, 239, linePointY + 31);
+        bzero(buff, 15360);
+      }
+      for (uint8_t i = 0; i < 16; i++) {//16 or 24
+        uint16_t b2 = b1 + i * 480;
+        uint16_t m1 = mChars + i;
+        for (uint8_t j = 0; j < 16; j++) {
+          // 16 * 16
+          // if (((0x80 >> j % 8) & ubuff.buff[m * 32 + i * 2 + (j >> 3)]) != 0) {
+          //   myGLCD.drawPixel((c % 15) * 16 + j, (c / 15) * 16 + i);
+          // 24 * 24
+          // if ((i1 & ubuff.fbuff[m * 24 + j][i2]) != 0) {
+          //   myGLCD.drawPixel((c % 10) * 24 + j, (c / 10) * 24 + i);
+          uint16_t b3 = b2 + j * 2;
+          if (((0x80 >> j % 8) & ubuff.fbuff16[m1][j>>3]) != 0) {
+            buff[b3] = 0xFF;
+            buff[b3 + 1] = 0xFF;
+          }
+        }
+      }
+      //draw font pix
+      if (m % 30 == 29) {
+        sbi ( myGLCD.P_RS, myGLCD.B_RS );
+        SPI.writeBytes(buff, 15360);
+      }
+      //c++;
+      // Serial.print(F("2222\t"));
+      // Serial.println(c);
+      if (m == 299) { //299 for 16, 129 for 24
+        Serial.println(F("3333"));
+        myGLCD.setColor(VGA_RED);
+        myGLCD.setBackColor(VGA_BLUE);
+        myGLCD.print("Print:", CENTER, 230);
+        myGLCD.printNumI((millis() - start), CENTER, 250);
+        start = millis();
+        myGLCD.setFont(SevenSegNumFont);
+        myGLCD.printNumI(pos, CENTER, 269);
+        myGLCD.setColor(VGA_WHITE);
+        myGLCD.setBackColor(0, 0, 0);
+        myGLCD.setFont(BigFont);
+        //c = 0;
+        delay(500);
+        myGLCD.clrScr();
+        delay(5);
+        Serial.print(F("Heap Size:"));
+        Serial.print(ESP.getFreeHeap());
+        Serial.print(F("\tNext Pos:"));
+        Serial.println(pos);
+        Serial.println(F("4444"));
+      }
+      delay(5);
+    }
+  }
+}
+
+
 void dumpFont24() {
-  uint16_t x = 0, y = 0, c = 0;
-  uint8_t buff[240*2*24];//256 for 16;216 for 24 px
-  uint16_t bcolor = VGA_BLACK, fcolor[3] = {VGA_RED, VGA_GREEN, VGA_BLUE};
+  uint16_t x = 0, y = 0;//, c = 0;
+  //uint16_t bcolor = VGA_BLACK, fcolor[3] = {VGA_RED, VGA_GREEN, VGA_BLUE};
   pos = 14 + F16L;
   Serial.println(pos);
   unsigned long start = millis();
@@ -153,54 +242,81 @@ void dumpFont24() {
     pos += 9360;//256 for 16;216 for 24 px
     delay(40);
     FLASH_CS_OFF;
-    //    cbi(myGLCD.P_CS, myGLCD.B_CS);
+    cbi(myGLCD.P_CS, myGLCD.B_CS);
     //    myGLCD.setXY((c%15)*16,(c/15)*16,15,15);
+    //    uint16_t cidx = 0;
     for (uint8_t m = 0; m < 130; m++) {// 8 for 16; 3 for 24
       // for (uint8_t n = 0; n < 10; n++) {
       //   uint8_t z = m*n;
-        for (uint8_t i = 0; i < 24; i++) {//16 or 24
-          uint8_t i1 = 0x80 >> i % 8;
-          uint8_t i2 = i / 8;
-          for (uint8_t j = 0; j < 24; j++) {
-            // 16 * 16
-            // if (((0x80 >> j % 8) & ubuff.buff[m * 32 + i * 2 + (j >> 3)]) != 0) {
-            //   myGLCD.drawPixel((c % 15) * 16 + j, (c / 15) * 16 + i);
-            // 24 * 24
-            if ((i1 & ubuff.fbuff[m * 24 + j][i2]) != 0) {
-              myGLCD.drawPixel((c % 10) * 24 + j, (c / 10) * 24 + i);//10 char per line
-
-            }
+//      if (m % 10 == 0) {
+//        bzero(buff, 11520);
+//      }
+      uint16_t mChars = m * 24;
+      //      uint16_t linePointX = (m % 10) * 24;
+      uint16_t linePointY = (m / 10) * 24;
+      //      uint16_t ml = 0;
+      uint16_t b1 = (m % 10) * 48;
+      if (b1 == 0)
+      {
+        //        cidx = 0;
+        bzero(buff, 11520);
+        myGLCD.setXY(0, linePointY, 239, linePointY + 23);
+      }
+      for (uint8_t i = 0; i < 24; i++) {//16 or 24
+        uint8_t i1 = 0x80 >> i % 8;
+        uint8_t i2 = i / 8;
+        //        ml = linePointY + i;
+        uint16_t b2 = b1 + i * 480;
+        for (uint8_t j = 0; j < 24; j++) {
+          // 16 * 16
+          // if (((0x80 >> j % 8) & ubuff.buff[m * 32 + i * 2 + (j >> 3)]) != 0) {
+          //   myGLCD.drawPixel((c % 15) * 16 + j, (c / 15) * 16 + i);
+          // 24 * 24
+          // if ((i1 & ubuff.fbuff[m * 24 + j][i2]) != 0) {
+          //   myGLCD.drawPixel((c % 10) * 24 + j, (c / 10) * 24 + i);
+          uint16_t b3 = b2 + j * 2;
+          if ((i1 & ubuff.fbuff24[mChars + j][i2]) != 0) {
+            // myGLCD.drawPixel(linePointX + j, ml);//10 char per line
+            //y*30+(m%10)*3+y/8+y%8
+            buff[b3] = 0xFF;
+            buff[b3 + 1] = 0xFF;
           }
         }
-        //draw font pix
-        c++;
-        // Serial.print(F("2222\t"));
-        // Serial.println(c);
-        if (c == 130) { //300 for 16, 130 for 24
-          Serial.println(F("3333"));
-          myGLCD.setColor(VGA_RED);
-          myGLCD.setBackColor(VGA_TRANSPARENT);
-          myGLCD.print("Print:", CENTER, 230);
-          myGLCD.printNumI((millis() - start), CENTER, 250);
-          start = millis();
-          myGLCD.setFont(SevenSegNumFont);
-          myGLCD.printNumI(pos, CENTER, 269);
-          myGLCD.setColor(VGA_WHITE);
-          myGLCD.setBackColor(0, 0, 0);
-          myGLCD.setFont(BigFont);
-          c = 0;
-          delay(500);
-          myGLCD.clrScr();
-          delay(5);
-          Serial.print(F("Heap Size:"));
-          Serial.print(ESP.getFreeHeap());
-          Serial.print(F("\tNext Pos:"));
-          Serial.println(pos);
-          Serial.println(F("4444"));
-        }
-        delay(5);
       }
+      //draw font pix
+      if (m % 10 == 9) {
+        sbi ( myGLCD.P_RS, myGLCD.B_RS );
+        SPI.writeBytes(buff, 11520);
+      }
+      //c++;
+      // Serial.print(F("2222\t"));
+      // Serial.println(c);
+      if (m == 129) { //300 for 16, 129 for 24
+        Serial.println(F("3333"));
+        myGLCD.setColor(VGA_RED);
+        myGLCD.setBackColor(VGA_BLUE);
+        myGLCD.print("Print:", CENTER, 230);
+        myGLCD.printNumI((millis() - start), CENTER, 250);
+        start = millis();
+        myGLCD.setFont(SevenSegNumFont);
+        myGLCD.printNumI(pos, CENTER, 269);
+        myGLCD.setColor(VGA_WHITE);
+        myGLCD.setBackColor(0, 0, 0);
+        myGLCD.setFont(BigFont);
+        //c = 0;
+        delay(500);
+        myGLCD.clrScr();
+        delay(5);
+        Serial.print(F("Heap Size:"));
+        Serial.print(ESP.getFreeHeap());
+        Serial.print(F("\tNext Pos:"));
+        Serial.println(pos);
+        Serial.println(F("4444"));
+      }
+      delay(5);
     }
+    //    sbi(myGLCD.P_CS, myGLCD.B_CS);
+  }
   // }
 }
 
@@ -208,11 +324,11 @@ void dumpFont24() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // myGLCD.clrScr();
-  // sbi(myGLCD.P_CS, myGLCD.B_CS);
-  // delay(5);
-  // dumpFont16();
-  // delay(3000);
+  myGLCD.clrScr();
+  sbi(myGLCD.P_CS, myGLCD.B_CS);
+  delay(5);
+  dumpFont16Quick();
+  delay(3000);
   unsigned long start = millis();
   myGLCD.clrScr();
   Serial.print(F("Clean screen times:\t"));
